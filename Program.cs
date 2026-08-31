08493749880ecaee21af3ed1a71ea33661ab45e7
Marcus Löf <LeafMaster1@outlook.com>
2026-08-31 13:10:06 +0200

    add builder sevice singleton för fileactivityLogger

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using Features.Todos;
+<<<<<<< Updated upstream
 using Features.Todos.GetTodo;
+=======
+using Infrastructure.Logging;
+>>>>>>> Stashed changes
 using Infrastructure.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ITodoStore, TodoStore>();
+builder.Services.AddSingleton<IActivityLogger, FileActivityLogger>();
 
 var app = builder.Build();
 
