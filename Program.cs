8e1b8b6b66d352a0ec2baba35258b094d85e9413
discotomten <linkaan_lonn@hotmail.com>
2026-08-31 13:30:19 +0200

    lägger till swagger

--- Program.cs
+++ Program.cs
@@ -11,7 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<ITodoStore, TodoStore>();
 builder.Services.AddSingleton<IActivityLogger, FileActivityLogger>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
